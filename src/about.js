d8ece30507d8ca873231828d615078d47fe1fab8
kumenger <44558777+kumenger@users.noreply.github.com>
2022-05-13 20:34:49 -0600

    try

--- src/about.js
+++ src/about.js
@@ -1,60 +1,72 @@
-import { Card, Figure } from "react-bootstrap";
-import SkillsComponent  from "./SkillsCOmponent"
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+
 const About = () => {
- 
-  return ( <div className="" >
-    <div className=" a  " >
-    <div className="row"><div className="12 text-center"> <p className=" heading-main" style={{letterSpacing:"13px",fontFamily:"Baloo Bhai"}}> KUMENGER BEYENE  </p></div></div>
-    <div className="row  aboutholder  " >
+  return (
+    <div className="">
       
-        <div className=" col-xs-12 col-s-12 offset-md-2 col-md-4 bounce-1  d-flex justify-content-center" >
-       
-  <img  src="https://i.ibb.co/9tGLc6C/My-project-5-modified.png"  style={{width:"80%",height:"",objectFit:"contain"}} />
- 
- </div>
-  <div className="  col-xs-12   col-s-12 col-md-4  heading-sun "  >
-    <p class=" abouttext" >
-             
-          I'm <strong className="pink">a fullstack
-             Web,Mobile Developer </strong>  from{" "}
-              <a href="https://en.wikipedia.org/wiki/Ethiopia" target="_blank" className="link">
+      <div className=" a  ">
+        <div className="row">
+          <div className="12 text-center">
+            {" "}
+            <p
+              className=" heading-main"
+              style={{ letterSpacing: "13px", fontFamily: "Baloo Bhai" ,color:"turquoise"}}
+            >
+              {" "}
+             <strong>KUMENGER BEYENE</strong> 
+            </p>
+          </div>
+          <br></br><br></br>
+        </div>
+        <div className="row  aboutholder  ">
+          <div className=" col-xs-12 col-s-12 offset-md-2 col-md-3 bounce-1  d-flex justify-content-center ">
+            <img
+              src="https://i.ibb.co/9tGLc6C/My-project-5-modified.png"
+              style={{ width: "80%", height: "", objectFit: "contain" }}
+            />
+          </div>
+          <div className="  col-xs-12   col-s-12  col-md-4  heading-sun ">
+            <p class=" abouttext">
+              I'm{" "}
+              <strong className="pink">
+                a fullstack Web,Mobile Developer{" "}
+              </strong>{" "}
+              from{" "}
+              <a
+                href="https://en.wikipedia.org/wiki/Ethiopia"
+                target="_blank"
+                className="link"
+              >
                 Ethiopia
-              </a>{" "}<br></br>
+              </a>{" "}
+              <br></br>
               one of the lucky ones to Won the{" "}
               <a
                 href="https://en.wikipedia.org/wiki/Diversity_Immigrant_Visa"
-                target="_black" className="link"
+                target="_black"
+                className="link"
               >
                 Diversity visa lottery
               </a>{" "}
-              that gives you the opportunity to <strong className="pink">Permanent Resident Card</strong>.<br></br>  Graduated in Computer Science and
-              Information Techonolgy,<strong className="pink">Bsc</strong> at{" "}
-              <a href="https://www.amu.edu.et/" target="_blank" className="link">
+              that gives you the opportunity to{" "}
+              <strong className="pink">Permanent Resident Card</strong>.
+              <br></br> Graduated in Computer Science and Information
+              Techonolgy,<strong className="pink">Bsc</strong> at{" "}
+              <a
+                href="https://www.amu.edu.et/"
+                target="_blank"
+                className="link"
+              >
                 <font>Arba Minch University</font>
               </a>{" "}
             </p>
-           
-            
-            
-
-                    
-              
-   
-                  
-            
-                 
-                 
-                 
-</div>
-
-
-
+          </div>
         </div>
-       
-     
-        
-        </div>
-       
-        </div>);
+      </div>
+    </div>
+  );
 };
 export default About;
--- src/header.js
+++ src/header.js
@@ -2,53 +2,53 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import App from "./App";
-import { BrowserRouter as Router,Route, Routes ,Link} from "react-router-dom";
-import Project from "./projects";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  
+} from "react-router-dom";
 
+import About from './about'
 
 
 const Header = () => {
   return (
     <div className="container">
       <div className="">
-        <Navbar collapseOnSelect expand="lg" className="c ">
-          <Container fluid>
+      <Navbar collapseOnSelect expand="lg" className="c ">
+          <Container>
             <Navbar.Brand as={Link} to="/">
-              <p className="navitems">About Me</p>{" "}
+              <p className="navitems btn   about " eventKey={1}>
+                About
+              </p>{" "}
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="ms-auto">
-               
-           <Nav.Link  as={Link} to='/projects' >
-                  <p className="navitems">Projects</p>
+                <Nav.Link as={Link} to="/skills" eventKey={3} className="">
+                  <p className="navitems btn about ">Skills</p>
                 </Nav.Link>
-                
-                <Nav.Link  className="">
-                  <p className="navitems">Resume</p>
+                <Nav.Link className="">
+                  <p className="navitems btn about">Resume</p>
                 </Nav.Link>
 
                 <Nav.Link href="#">
-                  <p className="navitems">Award</p>
+                  <p className="navitems btn about">Award</p>
                 </Nav.Link>
-                <Nav.Link href="#" className="navitems">
-                  <p className="navitems">Hire Me</p>
+                <Nav.Link as={Link} to="/contact" eventKey={4}>
+                  <p className="navitems btn about ">Contact</p>
                 </Nav.Link>
-                <Nav.Link href="#" className="navitems">
-                  <p className="navitems"> Contac</p>
+                <Nav.Link as={Link} to="/projects" eventKey={2}>
+                  <p className="navitems btn   about">Projects</p>
                 </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
       </div>
-      <div>
-      <Routes>
-            <Route exact path="/"  element={<App/>} />
-            <Route path="/projects" element={<Project/>} />
-         </Routes>
-      </div>
-     
+   
     </div>
   );
 };
